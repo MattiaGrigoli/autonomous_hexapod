4d102fe5f0e00ee964776d9f620dcbce83235f15
MattiaGrigoli <mattiagrigoli03@gmail.com>
2026-08-13 21:15:27 +0000

    fixed compilation errors, added friction and dampening to joints, fixed file links

---
+++ src/freenove_description/launch/gazebo.launch.py
@@ -0,0 +1,66 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_freenove_description = get_package_share_directory('freenove_description')
+
+    set_gz_resource_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=[os.path.join(pkg_freenove_description, '..')]
+    )
+    
+    urdf_file = os.path.join(pkg_freenove_description, 'urdf', 'robot.urdf')
+
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'hexapod',
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.3',       
+            '-R', '1.5708',
+            '-P', '0.0',      
+            '-Y', '0.0'        
+        ],
+        output='screen'
+    )
+
+    # 5. NODO: Bridge per sincronizzare il tempo tra ROS 2 e Gazebo
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        set_gz_resource_path,
+        robot_state_publisher,
+        gazebo,
+        spawn_robot,
+        bridge
+    ])
